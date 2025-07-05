8623aafb819a2cec0d3715738cde09b7988b4975
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-07-06 03:06:59 +0600

    Image added

--- src/app/(pages)/blogs/page.jsx
+++ src/app/(pages)/blogs/page.jsx
@@ -1,10 +1,27 @@
 import style from "@/app/(pages)/blogs/blogs.module.css"
+import Image from "next/image";
+import img_01 from "../../../../public/images/doraemon.jpg"
+import img_02 from "../../../../public/images/doraemon_2.jpg"
 
 const Blogs = () => {
     return (
         <div>
             <div className="container mx-auto w-full px-2 sm:px-6 lg:px-8 py-10">
                 <h1 className={`${style.common_heading}`}>Hello, Blogs page</h1>
+
+                <div className="grid gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 mt-10">
+                    <div className="border rounded-md p-3 text-center">
+                        <h1>Walidur Tanjim</h1>
+                    </div>
+
+                    <div className="border rounded-md p-3 text-center relative h-[250px]">
+                        <Image src={img_01} fill={true} quality={100} priority={false} placeholder="blur" blurDataURL="" alt="Card Image" className="w-full h-[250px] rounded-md" />
+                    </div>
+
+                    <div className="border rounded-md p-3 text-center relative h-[250px]">
+                        <Image src={img_02} fill={true} quality={100} priority={false} placeholder="blur" blurDataURL="" alt="Card Image" className="w-full h-[250px] rounded-md" />
+                    </div>
+                </div>
             </div>
         </div>
     );
